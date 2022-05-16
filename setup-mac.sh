25b08074039d0145ce2a39bd2345fbb7176e79d7
Peter Gill <peter@majorsilence.com>
2022-05-16 13:55:30 -0230

    Create setup-mac.sh

---
+++ setup-mac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# install brew package management
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask flameshot
+
+
+# install vm software
+brew install qemu utm
+
+
