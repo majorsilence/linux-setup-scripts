314adc8b634dff3f87249841e1bb4280158c9183
Peter Gill <peter@majorsilence.com>
2022-06-03 07:53:58 -0230

    Update setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -4,7 +4,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install --cask flameshot
-
+brew install --cask vscodium
 
 # install vm software
 brew install qemu utm
