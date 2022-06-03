f932a3708dd320571c11b9e752d2ccc3379467c1
Peter Gill <peter@majorsilence.com>
2022-06-03 08:03:54 -0230

    Update setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -5,12 +5,16 @@
 
 brew install --cask flameshot
 brew install --cask vscodium
+brew install --cask dotnet-sdk
 brew install --cask rider
 brew install --cask zoom
 brew install --cask firefox
 brew install --cask obs
 brew install --cask libreoffice
-
+brew install --cask postman
+brew install --cask powershell
+brew install git
+brew install --cask github
 
 # install vm software
 brew install qemu utm
